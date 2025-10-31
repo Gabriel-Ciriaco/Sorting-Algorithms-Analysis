31a36f14d62cfa493b53edfc869c9f558f759e9d
Carol-Nunes <cacanunesaraujo@gmail.com>
2025-10-30 22:09:46 -0300

    Create shell_sort.c
    Algoritmo Shell Sort criado.

---
+++ C_Algorithms_Library/algorithms/shell_sort.c
@@ -0,0 +1,21 @@
+void shell_sort(int *vetor, int n, int *vetorGaps, int numGaps)
+{
+    int pos, i, j, span, chave;
+
+    for(pos = 0; pos < numGaps; pos++)
+    {
+        span = vetorGaps[pos];
+
+        for(i = span; i < n; i++)
+        {
+            chave = vetor[i];
+
+            for(j = i - span; j >= 0 && vetor[j] > chave; j -= span)
+            {
+                vetor[j + span] = vetor[j];
+            }
+
+            vetor[j + span] = chave;
+        }
+    }
+}
