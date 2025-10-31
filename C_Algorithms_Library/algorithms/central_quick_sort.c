796ceb8dff662b1163bd20f625d2cb0004effccb
Carol-Nunes <cacanunesaraujo@gmail.com>
2025-10-30 22:24:26 -0300

    Create central_quick_sort.c
    Algoritmo Quick Sort com pivô sendo o elemento central da lista criado.

---
+++ C_Algorithms_Library/algorithms/central_quick_sort.c
@@ -0,0 +1,47 @@
+#include "sort_algorithms.h"
+
+int particionar(int *vetor, int ini, int fim)
+{
+    int meio = (ini + fim) / 2;
+
+    troca(&vetor[meio], &vetor[ini]);
+
+    int pivo = vetor[ini];
+
+    int i = ini;
+
+    int j = fim;
+
+    while (i < j)
+    {
+        while(vetor[i] <= pivo && i <= fim - 1)
+        {
+            i++;
+        }
+
+        while(vetor[j] > pivo && j >= ini + 1)
+        {
+            j--;
+        }
+
+        if (i < j)
+        {
+            troca(&vetor[i], &vetor[j]);
+        }
+    }
+
+    troca(&vetor[ini], &vetor[j]);
+
+    return j;
+}
+
+void central_quick_sort(int *vetor, int ini, int fim)
+{
+    if (ini < fim)
+    {
+        int pos = particionar(vetor, ini, fim);
+
+        central_quick_sort(vetor, ini, pos - 1);
+        central_quick_sort(vetor, pos + 1, fim);
+    }
+}
